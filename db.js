99ced391ea59f661b1b62843b2f128948bf44a20
Niraj Kumar <nkumar@tacme.com>
2016-05-03 11:07:43 +0400

    Update for production

--- db.js
+++ db.js
@@ -1,30 +1,36 @@
 var Sequelize = require('sequelize');
-// var env = 'production';
-var env = 'development';
+var env = 'production';
+// env = 'development';
 var sequelize;
+try {
+	if(env === 'production'){
+		console.log('connecting to databse in heroku');
+		sequelize = new Sequelize('postgres://cnhalpbaygixbq:XOD2WObhEjdOOQYpAN5osYkwT5@ec2-54-243-63-195.compute-1.amazonaws.com:5432/d905npbrc1ccs');
+	}else{
+		console.log('connecting to databse in local');
+		sequelize = new Sequelize('postgres://postgres:nk@localhost:5433/tacme?ssl=true');	
+	}
 
-if(env === 'production'){
-	console.log('1connecting to databse in heroku');
-	sequelize = new Sequelize('postgres://cnhalpbaygixbq:XOD2WObhEjdOOQYpAN5osYkwT5@ec2-54-243-63-195.compute-1.amazonaws.com:5432/d905npbrc1ccs');
-}else{
-	console.log('2connecting to databse in local');
-	sequelize = new Sequelize('postgres://postgres:nk@localhost:5433/tacme?ssl=true');	
+	sequelize.authenticate()
+	    .then(function(success) {
+	    console.log('----------------------------- Database Connection Established Successfully. ---------------------------------')
+	  }, function (err) { 
+	    console.log('Unable to connect to the database:', err);
+	    console.log(err);
+	    console.log('----------------------------- err begin ---------------------------------')
+	  }).catch(function (err) {
+	        console.log('----------------------------- err in catch begin ---------------------------------')
+	        console.log(err);
+	        console.log('----------------------------- err in catch end ---------------------------------')
+	    })
+	    .done();
+	var db = {};
+	db.user = sequelize.import(__dirname + '/models/users.js'); 
+
+	db.sequelize = sequelize;  
+} catch(e) {
+	console.log(e);
 }
 
-sequelize.authenticate()
-    .then(function () {
-        console.log("CONNECTED! ");
-    })
-    .catch(function (err) {
-        console.log("SOMETHING DONE GOOFED");
-        console.log(err);
-    })
-    .done();
-
-
-var db = {};
-db.user = sequelize.import(__dirname + '/models/users.js'); 
-
-db.sequelize = sequelize;  
 
 module.exports = db;
